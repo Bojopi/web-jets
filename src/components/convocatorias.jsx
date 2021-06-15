d17b9ae833c4f1f3e6992ce20599342246d8a527
Bryana Ojopi <ojopibryana5@gmail.com>
2021-06-14 21:35:34 -0400

    ya no funciono el heroku

---
+++ src/components/convocatorias.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../css/convocatorias.css'
+
+
+const Convocatorias = () => {
+    return(
+        <div className="cont-convocatorias">
+
+        </div>
+    )
+}
+
+export default Convocatorias
