044d6b0c72d4b9140d8e0572d612ff4925ac7e27
Bryana <ojopibryana5@gmail.com>
2021-06-16 16:09:37 -0400

    cambios de nuevo en el cronograma 3

--- src/components/info_jets.jsx
+++ src/components/info_jets.jsx
@@ -228,7 +228,7 @@ const Informacion = () => {
             </div>
           </div>
           <div className="buttons">
-            <a href="https://drive.google.com/file/d/1_o_KiIhnMDM60aE-PuZeBa5jZU5GbttB/view?usp=sharing" className="programa btn">
+            <a href="https://drive.google.com/file/d/148b7fKNj-dAJeHu_nU3b1nU4WWkIhOJb/view?usp=sharing" className="programa btn">
               PROGRAMA
             </a>
             <Link to={"/jets/memorias"} className="memorias btn">
