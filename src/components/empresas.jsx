d7ec4a198410aa21f4bca99130e47e3db5fe815b
Bryana <ojopibryana5@gmail.com>
2021-06-23 14:41:08 -0400

    se arregló el scroll de las empresas

--- src/components/empresas.jsx
+++ src/components/empresas.jsx
@@ -95,6 +95,87 @@ const Empresas = () => {
                 alt=""
               />
             </div>
+
+
+            {/* GRUPO 2 */}
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623717856/jets/empresas/FARMACORP_LOGO_nm1g7m.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623158711/jets/empresas/DOLLY_LOGO_stxwc9.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623157059/jets/empresas/LILIANA_CASTELLANOS_LOGO_smyexu.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623158711/jets/empresas/COCA_COLA_LOGO_imamkw.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623436507/jets/empresas/TIGO_LOGO_qdc0m2.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1622986943/jets/empresas/SIRENA_LOGO_zzxoju.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623436507/jets/empresas/BANCO_GANADERO_LOGO_udt3yy.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1622986942/jets/empresas/CAINCO_LOGO_gzhyxn.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1622986942/jets/empresas/CHAVEZ_LOGO_egoq89.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623784746/jets/empresas/santa_rosa_club_xnkoo8.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623788882/jets/empresas/Captura_ggmkdb.png"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1623785530/jets/empresas/ministerio2_za7wao.jpg"
+                alt=""
+              />
+            </div>
+            <div className="listas">
+              <img
+                src="https://res.cloudinary.com/utepsajets/image/upload/v1624382875/jets/empresas/GRUPO_LA_PAPELERA_hlejto.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
