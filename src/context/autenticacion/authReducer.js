5269608869cb0d015944d5cb2e1e11bf81c997a1
Bryana <ojopibryana5@gmail.com>
2021-06-28 16:43:44 -0400

    intentando que funcione el login

---
+++ src/context/autenticacion/authReducer.js
@@ -0,0 +1,13 @@
+import { 
+    OBTENER_USUARIO,
+    LOGIN_EXITOSO,
+    LOGIN_ERROR,
+    CERRAR_SESION
+} from '../../types'
+
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
