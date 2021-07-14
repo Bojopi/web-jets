ddbe657b8d25b6c3d8dfabc55002e5df692387d6
Bryana <ojopibryana5@gmail.com>
2021-07-14 16:00:48 -0400

    prueba 34 y no se cuanto

--- src/components/lectorPDF.jsx
+++ src/components/lectorPDF.jsx
@@ -1,36 +1,94 @@
 import React, { useContext, useEffect } from "react";
-import PdfContext from '../context/autenticacion/authContext'
+import PdfContext from "../context/autenticacion/authContext";
 
-import { jsPDF } from 'jspdf'
+import { jsPDF } from "jspdf";
+import {
+  PDFDownloadLink,
+  Page,
+  Text,
+  Image,
+  View,
+  Document,
+  StyleSheet,
+} from "@react-pdf/renderer";
 
 import "../css/lector.css";
 
+const styles = StyleSheet.create({
+  texto: {
+    textAlign: "center",
+    position: "absolute",
+    top: 500,
+    width: 100,
+  },
+  imagen: {
+    width: 100,
+  },
+});
 
 const Descargar = () => {
-
   //Extraer la info del usuario
-  const pdfContext = useContext(PdfContext)
-	const { usuario, usuarioAutenticado } = pdfContext
+  const pdfContext = useContext(PdfContext);
+  const { usuario, usuarioAutenticado } = pdfContext;
 
   useEffect(() => {
-    usuarioAutenticado()
-  }, [])
+    usuarioAutenticado();
+  }, []);
 
-  const doc = new jsPDF()
-  doc.html(document.body, {
-    callback: (doc) => {
-      doc.save()
-    },
-    x: 10,
-    y: 10
-  })
+  const Doc = () => (
+    <Document>
+      <Page size="LETTER">
+        <View>
+          <Image
+            src="https://i.postimg.cc/PqB4H7RQ/CERTIFICADO-ESTUDIANTES.png"
+            style
+          ></Image>
+          <Text style={styles.texto}>
+            {usuario.name} {usuario.lastname}
+          </Text>
+        </View>
+      </Page>
+    </Document>
+  );
 
   return (
-    <div className="fondo" id="fon">
-        <img className="imagen" />
-            {usuario ? <h1 className="nomUsuario">{usuario.name} {usuario.lastname}</h1> : null}
+    <div className="fondo">
+      <PDFDownloadLink document={<Doc />} fileName="certificado.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? "Loading document..." : "Download Now!"
+        }
+      </PDFDownloadLink>
     </div>
   );
 };
 
 export default Descargar;
+
+// const Descargar = () => {
+
+//   //Extraer la info del usuario
+//   const pdfContext = useContext(PdfContext)
+// 	const { usuario, usuarioAutenticado } = pdfContext
+
+//   useEffect(() => {
+//     usuarioAutenticado()
+//   }, [])
+
+//   const doc = new jsPDF()
+//   doc.html(document.body, {
+//     callback: (doc) => {
+//       doc.save()
+//     },
+//     x: 10,
+//     y: 10
+//   })
+
+//   return (
+//     <div className="fondo" id="fon">
+//         <img className="imagen" />
+//             {usuario ? <h1 className="nomUsuario">{usuario.name} {usuario.lastname}</h1> : null}
+//     </div>
+//   );
+// };
+
+// export default Descargar;
